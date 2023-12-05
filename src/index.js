1108ccb37e7684d351ec543b6bbeb20f46586201
dev-carl-robert <dev.carl.robert@gmail.com>
2023-12-05 00:46:27 -0300

    desafio do Felipão

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
-console.log("oi");
+let nome = "Carl";
+let xp = 6000;
+let nivel;
+
+if (xp < 1000) {
+    nivel = "Ferro";
+}
+else if (xp > 1000 && xp <= 2000) {
+    nivel = "Bronze";
+}
+else if (xp > 2000 && xp <= 5000) {
+    nivel = "Prata";
+}
+else if (xp > 5000 && xp <= 7000) {
+    nivel = "Ouro";
+}
+else if (xp > 7000 && xp <= 8000) {
+    nivel = "Platina";
+}
+else if (xp > 8000 && xp <= 9000) {
+    nivel = "Ascendente";
+}
+else if (xp > 9000 && xp <= 10000) {
+    nivel = "Imortal";
+}
+else {
+    nivel = "Radiante";
+}
+
+console.log(`O héroi de nome ${nome} está no nivel ${nivel}`);
